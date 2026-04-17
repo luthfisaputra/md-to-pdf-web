0a36fe4ab8778a3958473fab41d08fac8fca9760
Luthfi Saputra <luthfsptr@gmail.com>
2026-04-17 23:36:08 +0700

    refactor: method generatePdfFromHtml

--- src/main/java/com/bro/markdown/markdown_to_pdf/service/PdfService.java
+++ src/main/java/com/bro/markdown/markdown_to_pdf/service/PdfService.java
@@ -18,10 +18,15 @@ public class PdfService {
             String fullHtml = "<!DOCTYPE html>\n" +
                     "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
                     "<head>\n" +
-                    "<meta charset=\"UTF-8\" />\n" + // HARUS asa slash penutup />
+                    "<meta charset=\"UTF-8\" />\n" + // HARUS ada slash penutup />
                     "<style>\n" +
                     "body { font-family: sans-serif; padding: 40px; line-height: 1.6; }\n" +
                     "h1, h2, h3 { color: #333; }\n" +
+
+                    "code { background-color: #f6f8fa; padding: 2px 5px; border-radius: 4px; font-family: JetBrains Mono; font-size: 14px; color: #24292f; }\n" +
+                    "pre { background-color: #f6f8fa; padding: 15px; border-radius: 6px; border: 1px solid #dcdcdc; white-space: pre-wrap; word-wrap: break-word; }\n" +
+                    "pre code { background-color: transparent; padding: 0; white-space: pre-wrap; }\n" +
+
                     "</style>\n" +
                     "</head>\n" +
                     "<body>\n" +
