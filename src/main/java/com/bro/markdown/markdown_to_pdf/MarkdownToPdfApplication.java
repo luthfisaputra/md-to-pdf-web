ee086c2d5773257e1ea34fb343ec4af43d4b58f0
Luthfi Saputra <luthfsptr@gmail.com>
2026-04-17 20:25:10 +0700

    feat: add class MarkdownService in package service

---
+++ src/main/java/com/bro/markdown/markdown_to_pdf/MarkdownToPdfApplication.java
@@ -0,0 +1,13 @@
+package com.bro.markdown.markdown_to_pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkdownToPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkdownToPdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bro/markdown/markdown_to_pdf/MarkdownToPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bro.markdown.markdown_to_pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkdownToPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
